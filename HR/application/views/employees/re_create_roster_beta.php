a76247f0b2a51a02edf2ba07fa0a22e4e322a749
adityakohli467 <adityakohli467@gmail.com>
2026-06-29 06:29:40 +0530

    Show weekly Mon-Sun validation message + restrict picker on recreate roster views

--- HR/application/views/employees/re_create_roster_beta.php
+++ HR/application/views/employees/re_create_roster_beta.php
@@ -337,6 +337,9 @@
 		        }else if(data=='validation'){
 		         $msg = "Ensure all mandatory fields are populated";
 		         $icon = "warning";
+		        }else if(data=='weekvalidation'){
+		         $msg = "Roster must be exactly one week: start on Monday and end on Sunday.";
+		         $icon = "warning";
 		        }
 		         <!--start 6Jan 2021 work-->
 		         
@@ -438,10 +441,20 @@
         </script>
 	<script type="text/javascript">
             $(function () {
-                $('.datetimepicker1').datetimepicker({
+                $("input[name='start_date']").closest('.datetimepicker1').datetimepicker({
 					format: 'DD-MM-YYYY',
-					<!--minDate:new Date()-->
+					daysOfWeekDisabled: [0,2,3,4,5,6]
 				});
+                $("input[name='end_date']").closest('.datetimepicker1').datetimepicker({
+					format: 'DD-MM-YYYY',
+					daysOfWeekDisabled: [1,2,3,4,5,6]
+				});
+                $("input[name='start_date']").closest('.datetimepicker1').on('dp.change', function(e){
+                    if(e.date){
+                        var end = e.date.clone().add(6,'days');
+                        $("input[name='end_date']").closest('.datetimepicker1').data('DateTimePicker').date(end);
+                    }
+                });
             });
         </script>
 	<style>
